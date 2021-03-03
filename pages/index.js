10fbc3512846dd7fc1412e91d8ae71f820acc754
herny14 <hernychristine14@gmail.com>
2021-03-03 13:37:22 +0700

    test mui

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,14 @@ function Copyright() {
 }
 
 export default function App() {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  
   return (
     <Container maxWidth="sm">
       <Box my={4}>
